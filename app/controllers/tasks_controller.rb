781bc675bc566557716ee786c869d3ec20277b1c
yuru.secret <yuru.secret@gmail.com>
2021-09-25 08:56:34 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -50,7 +50,7 @@ before_action :set_task, only: [:show, :edit, :update, :destroy]
         @task = Task.find(params[:id])
     end
     def task_params
-        params.require(:task).permit(:content)
+        params.require(:task).permit(:content, :status)
     end
     
 end
