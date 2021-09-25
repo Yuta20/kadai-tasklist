0624909aac4b7eb3fd5113f37e0f52c8cda1eb4d
yuru.secret <yuru.secret@gmail.com>
2021-09-25 08:39:38 +0000

    共通化

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,12 @@
 class TasksController < ApplicationController
 
+before_action :set_task, only: [:show, :edit, :update, :destroy]
+
     def index
         @tasks = Task.all
     end
     
     def show
-        @task = Task.find(params[:id])
     end
     
     def new
@@ -24,7 +25,6 @@ class TasksController < ApplicationController
     end
     
     def edit
-        @task = Task.find(params[:id])
     end
     
     def update
@@ -46,7 +46,9 @@ class TasksController < ApplicationController
     end
     
     private
-    
+    def set_task
+        @task = Task.find(params[:id])
+    end
     def task_params
         params.require(:task).permit(:content)
     end
